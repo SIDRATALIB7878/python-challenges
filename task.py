c7cacc413b7f71a40acae86d134da731ba98ab61
SIDRATALIB7878 <164734030+SIDRATALIB7878@users.noreply.github.com>
2025-03-02 06:38:39 +0500

    Add files via upload

---
+++ task.py
@@ -0,0 +1,39 @@
+# TASK 01 PRINT A MESSAGE
+print("Hello world!welcome to python programming.\n Let's start coding!")
+
+# TASK 02 AI GREETING
+
+emoji = "😊"
+name = input("Enter your name: ")
+print(f"{emoji} Greeting, {name}! Welcome to the world of Python. Let's start coding!")
+
+# TASK 03 smart calculater
+def calculator():
+    print("Select an operation:")
+    print("1. Addition (+)")
+    print("2. Subtraction (-)")
+    print("3. Multiplication (*)")
+    print("4. Division (/)")
+    
+    choice = input("Enter the number of the operation you want to perform (1/2/3/4): ")
+    
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        
+        if choice == '1':
+            print(f"Result: {num1} + {num2} = {num1 + num2}")
+        elif choice == '2':
+            print(f"Result: {num1} - {num2} = {num1 - num2}")
+        elif choice == '3':
+            print(f"Result: {num1} * {num2} = {num1 * num2}")
+        elif choice == '4':
+            if num2 != 0:
+                print(f"Result: {num1} / {num2} = {num1 / num2}")
+            else:
+                print("Error: Division by zero is not allowed.")
+    else:
+        print("Invalid input. Please enter a valid operation number.")
+
+# Run the calculator
+calculator()
